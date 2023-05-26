632455f563bb6f1d9d3783f7f22a5a71998ae0e2
Erick Mora <m.erickpeligrogmail.com>
2023-05-25 19:45:12 -0600

    first commit course

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './img/rick-morty.png';
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+
+function App() {
+
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () =>{
+    const api = await fetch('https://rickandmortyapi.com/api/character')
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results)
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+
+        {characters ? (
+          
+        <Characters characters={characters} setCharacters={setCharacters} />
+        ) :
+        <>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          </p>
+          <button onClick={reqApi} className="btn-search">
+            Buscar personajes
+          </button>
+        </>
+      }
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
